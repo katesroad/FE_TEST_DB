297e0d8512c60798d4feafa7e19011fc21674939
Kate <test@gmail.com>
2021-06-06 18:27:09 -0400

    rename samples as test_react

---
+++ test_react/hooks/src/components/Todo/index.tsx
@@ -0,0 +1,19 @@
+import * as React from 'react'
+import { useGetTodo } from '../../hooks/json.hooks'
+
+export const Todo: React.FC<{ id: number }> = ({ id }) => {
+  const { isError, isSuccess, isLoading, data } = useGetTodo(id)
+  if (isLoading) return <p aria-label="loading">loading...</p>
+  if (isError) return <p role="alert">failed to load todo</p>
+  if (isSuccess) {
+    return (
+      <div>
+        <h4>{data?.title}</h4>
+        <p>ID: ${data?.id}</p>
+        <p>Completed: {data?.completed ? 'yes' : 'no'}</p>
+        <p>user: {data?.userId}</p>
+      </div>
+    )
+  }
+  return null
+}
