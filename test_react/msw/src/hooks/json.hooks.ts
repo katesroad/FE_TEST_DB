c2c3a3670058df530c784d716ee374fe731e42bd
Kate <test@gmail.com>
2021-06-09 13:44:26 -0400

    use url prefix based on enviroment

--- test_react/msw/src/hooks/json.hooks.ts
+++ test_react/msw/src/hooks/json.hooks.ts
@@ -2,28 +2,38 @@ import { useQuery, UseQueryResult } from 'react-query'
 import axios from 'axios'
 import { IToDo } from '../types'
 
+const isDev = process.env.NODE_ENV === 'development' ? true : false
+axios.defaults.baseURL = isDev
+  ? 'http://localhost:3000'
+  : process.env.REACT_APP_API_URL
+
+axios.interceptors.response.use(
+  (res) => {
+    try {
+      const { data, ok, msg } = res.data
+      if (ok) return data
+      else return Promise.reject(msg || 'unknown error')
+    } catch (e) {
+      return Promise.reject(e)
+    }
+  },
+  (e: unknown) => Promise.reject(e)
+)
+
 /**
  * Get todo by todo ID
  * @param id{number} the todo id
  * @returns todo{ITodo|null}
  */
 export function useGetTodo(id: number): UseQueryResult<IToDo | null> {
-  const queryFn = () => {
-    return axios
-      .get(`https://jsonplaceholder.typicode.com/todos/${id}`)
-      .then((res) => res.data as IToDo)
-  }
+  const queryFn = () => axios.get(`todos/${id}`)
   const queryKey = ['todos', id]
   return useQuery({ queryFn, queryKey })
 }
 
 // Get to do list
 export function useGetTodoList(): UseQueryResult<IToDo[] | null> {
-  const queryFn = () => {
-    return axios
-      .get(`https://jsonplaceholder.typicode.com/todos`)
-      .then((res) => res.data as IToDo)
-  }
+  const queryFn = () => axios.get(`todos`)
   const queryKey = ['todos']
   return useQuery({ queryFn, queryKey })
 }
--- test_react/msw/src/mocks/handlers.ts
+++ test_react/msw/src/mocks/handlers.ts
@@ -1,16 +1,25 @@
 import { rest } from 'msw'
 import { TODO_LIST } from './data'
 
+const isDev = process.env.NODE_ENV === 'development' ? true : false
+const API_HOST = isDev
+  ? 'http://localhost:3000'
+  : process.env.REACREACT_APP_API_URL
+
+const getUrl = (path: string) => `${API_HOST}/${path}`
+
 // Start the Mock Service Worker
 
 export const handlers = [
-  rest.get('/todos', (req, res, ctx) => {
-    ctx.json({
-      ok: true,
-      data: TODO_LIST,
-    })
+  rest.get(getUrl('todos'), (req, res, ctx) => {
+    return res(
+      ctx.json({
+        ok: true,
+        data: TODO_LIST,
+      })
+    )
   }),
-  rest.get('/todos:id', (req, res, ctx) => {
+  rest.get(getUrl('todos/:id'), (req, res, ctx) => {
     const { id } = req.params
     // the standard usage of response
     // Doc: https://mswjs.io/docs/api/response#standard-usage
@@ -20,7 +29,6 @@ export const handlers = [
         ctx.json({ ok: false, msg: 'Please provide an valid todo id.' })
       )
     }
-
     const todo = TODO_LIST.find((todo) => todo.id === +id)
     return res(
       ctx.status(200),
