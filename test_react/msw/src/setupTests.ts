79978eb664c38beb23fb737354c92adefd8d36c6
Kate <test@gmail.com>
2021-06-09 21:21:53 -0400

    setup test server at setupTest

--- test_react/msw/src/setupTests.ts
+++ test_react/msw/src/setupTests.ts
@@ -3,3 +3,16 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom'
+import { setupServer } from 'msw/node'
+import { handlers } from './mocks/handlers'
+
+const server = setupServer(...handlers)
+
+/**
+ * Setup the node server enviorment for testingg purpose
+ * Setup here is to avoid bug mentioned at this link: https://github.com/mswjs/msw/issues/397
+ */
+if (process.env.NODE_ENV === 'test') {
+  beforeAll(() => server.listen())
+  afterAll(() => server.close())
+}
