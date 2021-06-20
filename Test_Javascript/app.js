560d55b38da5d9c0d6b28f3d5ece759094255f75
Kate <test@gmail.com>
2021-06-20 18:19:28 -0400

    test sayHello and addNumbers function

--- Test_Javascript/app.js
+++ Test_Javascript/app.js
@@ -1,3 +1,8 @@
-module.exports = function () {
-  return 'hello'
+module.exports = {
+  sayHello() {
+    return 'Hello'
+  },
+  addNumbers(num1, num2) {
+    return Math.max(5, num1 + num2)
+  },
 }
--- Test_Javascript/test/app.test.js
+++ Test_Javascript/test/app.test.js
@@ -2,7 +2,25 @@ const { assert } = require('chai')
 const app = require('../app')
 
 describe('app', () => {
-  it('app should return hello', () => {
-    assert.equal(app(), 'hello')
+  describe('sayHello', () => {
+    const sayHelloResult = app.sayHello()
+    it('app.sayHello should return Hello', () => {
+      //   usually, we keep the result in a varaible as result
+      assert.equal(sayHelloResult, 'Hello')
+    })
+    it('app.sayHello should return string', () => {
+      const result = app.sayHello()
+      assert.typeOf(sayHelloResult, 'string')
+    })
+  })
+
+  describe('addNumbers', () => {
+    const addNumbersResult = app.addNumbers(5, 5)
+    it('app.addNumbers should be above 5', () => {
+      assert.isAbove(addNumbersResult, 5)
+    })
+    it('app.addNumbers should return type number', () => {
+      assert.typeOf(addNumbersResult, 'number')
+    })
   })
 })
