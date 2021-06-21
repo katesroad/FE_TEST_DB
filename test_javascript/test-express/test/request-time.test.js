340ecdafdedf358f0fc57223fcd6064f61bccacf
Kate <test@gmail.com>
2021-06-20 20:22:40 -0400

    update readme

--- test_javascript/test-express/test/request-time.test.js
+++ test_javascript/test-express/test/request-time.test.js
@@ -1,12 +1,14 @@
 const { assert } = require('chai')
 const requestTime = require('../libs/request-time')
+const sinon = require('sinon')
 
 describe('requestTime middleware', () => {
   it('should add a timestamp `requestTime` prop to `req`', () => {
     const req = {}
-    const next = () => {}
-    const res = () => {}
+    const res = sinon.fake()
+    const next = sinon.fake()
     requestTime(req, res, next)
-    assert.ok(req.requestTime > 0)
+	  assert.ok(req.requestTime > 0)
+	  assert.ok(next.tobecal)
   })
 })
